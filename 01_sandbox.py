4724b19c41eed52790c43048a304f1eb428dcb57
Jake-C00k <86276144+Jake-C00k@users.noreply.github.com>
2021-07-05 18:17:09 +1200

    Began play again

--- 01_sandbox.py
+++ 01_sandbox.py
@@ -2,11 +2,11 @@
 from random import randint
 
 #Set up Variables
-get_age = None
+get_age = 0
 prize = "🎁"
 no_prize = "😔"
-streak = None
-high_score = None
+streak = 0
+high_score = 0
 
 def instructions(): 
     input("We will ask your age and then generate a number, if your age is higher than this number, you will win a prize! ")
@@ -15,19 +15,19 @@ played_before = ("")
 #Checks if user has played before
 while played_before.lower().strip() != "yes":
     played_before = str(input("Have you played the age game before? ")).lower().strip()
-    if played_before == "no":
+    if played_before == "no" or played_before == "n":
         instructions()
         break
-    if played_before == "yes":
+    if played_before == "yes" or played_before == "y":
      break
     else:
         print("Sorry, there was an error, please re-enter and make sure your answer is a yes or no.")
         played_before = ("")
     
-input("Lets begin")
+secret = input("Lets begin. ").lower().strip()
 
 #Check to see whether the age is a valid number
-while get_age == None:
+while get_age == 0:
     try:
         get_age = int(input("Please enter your age as an integer."))
         while get_age <= 0 or get_age >= 123:
@@ -42,20 +42,34 @@ print("You are {} years old".format(get_age))
 min = get_age - 5
 max = get_age + 20
 
+sec_min = get_age - 20
+sec_max = get_age + 5
+
 #generate a random number
-for _ in range(1):
-	value = randint(min, max)
-	print("The number is... {}".format(value))
+if secret == "u u d d l r l r b a s":
+    for _ in range(1):
+        value = int(randint(sec_min, sec_max))
+        print("The number is...{}".format(value))
+else:
+    for _ in range(1):
+        value = int(randint(min, max))
+        print("The number is... {}".format(value))
+
 
 #Check whether the number is greater
 if value < get_age:
-    print("Hooray, you won!")
-    print(prize)
-    
+    print("Hooray, you won! ")
+    input(prize)
+    win = "yes"
+    streak += 1
 else:
-    print("Sorry, today isn't your lucky day.")
-    print(no_prize)
-    
+    print("Sorry, today isn't your lucky day. ")
+    input(no_prize)
+    win = "no"
 #Asks user if they would like to continue
+if win == "yes": 
+    play_again = input("Would you like to continue playing?")
+else: 
+    print("Thanks for playing the game, try again soon")
 
-
+#if play_again == "yes"
