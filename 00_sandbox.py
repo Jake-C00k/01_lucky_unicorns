775661abce906da97f00150c854af5aa62297471
Jake-C00k <cook.jake@awatapu.school.nz>
2021-07-05 13:01:10 +1200

    Result output

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -4,3 +4,5 @@ get_number = int(input("Choose a number? "))
 #Multiply the number by 5
 times_five = get_number * 5
 
+#Output the result
+print("{} times five is equal to {}".format(get_number, times_five))
