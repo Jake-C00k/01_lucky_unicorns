41985823744fc5eee1651d7ee247e28188b24702
Jake-C00k <cook.jake@awatapu.school.nz>
2021-07-09 10:55:40 +1200

    Variation 2

--- 01_yes_no_checker.py
+++ 01_yes_no_checker.py
@@ -1,11 +1,21 @@
-# Ask the user if they have played before
-played_before = input("Have you played this game before?").strip().lower()
-# If yes, continue program
-if played_before  == "yes" or played_before == "y":
-    print("Lets begin")
-# If no, display instructions
-elif played_before == "no" or played_before == "n":
-    print("Instructions")
-# If anything else
-else:
-    print("Please answer with a yes or no answer")
+#Set up functions
+def gamebeg():
+    valid = False
+    while not valid:
+    # Ask the user if they have played before
+        played_before = input("Have you played this game before?").lower().strip()
+    # If yes, continue program
+        if played_before  == "yes" or played_before == "y":
+            valid = True
+            print("Lets begin")
+        # If no, display instructions
+        elif played_before == "no" or played_before == "n":
+            valid = True
+            print("Instructions")
+        # If anything else
+        else:
+            print("Please answer with a yes or no answer")
+
+
+while "a" != "b":
+    gamebeg()
