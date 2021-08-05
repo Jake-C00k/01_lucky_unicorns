9b902e9f84224fbe378524be463c55a3509d49a2
Jake-C00k <cook.jake@awatapu.school.nz>
2021-08-06 10:24:14 +1200

    Token gen 1

--- Token_Gen.py
+++ Token_Gen.py
@@ -1,25 +1,17 @@
 from random import randint
 
-min = 1
-max = 100
-
-
 def num_gen():
     for _ in range(1):
-        value = int(randint(min, max))
+        value = int(randint(1, 100))
         if 0 < value <= 25:
             token = "Zebra"
-            statement = "Not bad, could be worse"
         elif 25 < value <= 50:
-            token = "Horse"
-            statement = "Not bad, could be worse"
+            token = "horse"
         elif 50 < value <= 90:
-            token = "😢😢 Donkey 😢😢"
-            statement = "Unlucky, hopefully you do better next time"
+            token = "donkey"
         elif 90 < value <= 100:
-            token = "🦄🦄 Unicorn 🦄🦄"
-            statement = "Wow, that's fantastic, you really won big!"
-    print("Generating token...")
+            token = "unicorn"
     print("You got a {}".format(token))
-    print(statement)
 
+while True:
+    num_gen()
