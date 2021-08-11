aa12a9c209cb4e5659ea64d7930199acdf12f716
Jake-C00k <cook.jake@awatapu.school.nz>
2021-08-11 14:24:05 +1200

    Finished variation 1

---
+++ 01_number_checker.py
@@ -0,0 +1,20 @@
+# Main routine
+error = "Please enter a whole number between 1 and 10\n"
+
+valid = False
+while not valid:
+    try:
+        # Ask the question
+        response = int(input("How much would you "
+                             "like to play with? "))
+        # if the response is too high / too low give
+        if 0 < response <= 10:
+            print("You have asked to play "
+                  "with ${}".format(response))
+
+        # output an error
+        else:
+            print(error)
+
+    except ValueError:
+        print(error)
